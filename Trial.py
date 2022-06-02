51398a6ddfa2651e5f95e18d14767ef31adbf29f
n01521239 <106199863+n01521239@users.noreply.github.com>
2022-06-02 19:52:42 -0400

    can create a repo using this code

--- Trial.py
+++ Trial.py
@@ -1,6 +1,6 @@
 
 from github import Github
-my_git = Github("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")
+my_git = Github("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")
 
 for repo in my_git.get_user().get_repos():
         print(repo.name)
@@ -9,3 +9,7 @@ wireless_project = my_git.get_repo("n01521239/wirelessproject")
 contents = wireless_project.get_contents("")
 for content_file in contents:
     print(content_file)
+
+user = my_git.get_user()
+repo = user.create_repo('test')
+repo.create_file("text.txt","commit","this is test")

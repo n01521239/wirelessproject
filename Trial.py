9374c78adcef3d8522a5235a5a7609c44a0018bb
n01521239 <106199863+n01521239@users.noreply.github.com>
2022-06-02 14:04:46 -0400

    python code to access contents of the repositories

--- Trial.py
+++ Trial.py
@@ -1,1 +1,11 @@
-print("hello")
+
+from github import Github
+my_git = Github("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")
+
+for repo in my_git.get_user().get_repos():
+        print(repo.name)
+
+wireless_project = my_git.get_repo("n01521239/wirelessproject")
+contents = wireless_project.get_contents("")
+for content_file in contents:
+    print(content_file)
